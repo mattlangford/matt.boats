14a2d949ac6ddf0e67ac5026e0cf1faf00962521
Matt Langford <matthew.langford95@gmail.com>
2022-05-05 23:14:22 -0400

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+WS_FILES=`git status --porcelain | wc -l`
+
+if [[ $WS_FILES -eq 1 ]]; then
+    echo "Dirty workspace! Commit all files and try again."
+    exit 1
+fi
+
+git checkout deploy
+git reset --hard $BRANCH_NAME
+cp dist/* .
+git add .
+git commit --amend --no-edit
+git push origin deploy -f
+git checkout $BRANCH_NAME
