ea39375922108a6bc256877b2d7abcb67e1c222d
Matt Langford <matthew.langford95@gmail.com>
2022-06-22 23:18:11 -0400

    Get tests passing for intersect

--- src/geom.rs
+++ src/geom.rs
@@ -2,6 +2,7 @@ use nalgebra as na;
 
 pub type Vector2f = na::Vector2<f32>;
 
+#[derive(Debug, Default)]
 pub struct Line {
     start: Vector2f,
     direction: Vector2f,
@@ -9,7 +10,7 @@ pub struct Line {
 }
 
 impl Line {
-    pub fn new_from_start_and_end(start: Vector2f, end: Vector2f) -> Line {
+    pub fn new_segment(start: Vector2f, end: Vector2f) -> Line {
         let diff = end - start;
         let length = diff.norm();
         Line::new(start, diff, length)
@@ -29,9 +30,9 @@ impl Line {
 }
 
 pub fn intersect_lines(a: &Line, b: &Line) -> Option<Vector2f> {
-    na::linalg::LU::new(na::Matrix2::<f32>::from_rows(&[
-        a.direction.transpose(),
-        -b.direction.transpose(),
+    na::linalg::LU::new(na::Matrix2::<f32>::from_columns(&[
+        a.direction,
+        -b.direction,
     ]))
     .solve(&(b.start - a.start))
     .filter(|tu| tu[0] >= 0.0 && tu[0] < a.length && tu[1] >= 0.0 && tu[1] < b.length)
@@ -44,11 +45,41 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_add() {
-        let a = Line::new_ray(na::vector![0.0, 0.0], na::vector![1.0, 0.0]);
-        let b = Line::new_ray(na::vector![1.0, 1.0], na::vector![0.0, -1.0]);
-        println!("{:?}", intersect_lines(&a, &b));
+    fn test_intersect() {
+        // parallel
+        assert_eq!(
+            intersect_lines(
+                &Line::new_ray(na::vector![0.0, 0.0], na::vector![1.0, 0.0]),
+                &Line::new_ray(na::vector![1.0, 1.0], na::vector![1.0, 0.0])
+            ),
+            None
+        );
 
-        assert_eq!(3, 2);
+        // simple
+        assert_eq!(
+            intersect_lines(
+                &Line::new_ray(na::vector![0.0, 0.0], na::vector![1.0, 0.0]),
+                &Line::new_ray(na::vector![1.0, 1.0], na::vector![0.0, -1.0])
+            ),
+            Some(na::vector![1.0, 0.0])
+        );
+
+        // too short
+        assert_eq!(
+            intersect_lines(
+                &Line::new(na::vector![0.0, 0.0], na::vector![1.0, 0.0], 0.1),
+                &Line::new_ray(na::vector![1.0, 1.0], na::vector![0.0, -1.0])
+            ),
+            None
+        );
+
+        // complex
+        assert_eq!(
+            intersect_lines(
+                &Line::new_segment(na::vector![4.0, 2.1], na::vector![-4.0, 4.1]),
+                &Line::new_segment(na::vector![-2.0, 1.1], na::vector![2.0, 5.1])
+            ),
+            Some(na::vector![0.0, 3.1])
+        );
     }
 }
